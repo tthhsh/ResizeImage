9462c40d9c704bf8819cb8372765d512ad758671
xinjian.jiang <tthhshjxj@gmail.com>
2014-05-05 17:30:25 +0800

    resize image demo

--- ResizeImage/ViewController.h
+++ ResizeImage/ViewController.h
@@ -9,5 +9,13 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
+@property (weak, nonatomic) IBOutlet UISlider *topSlider;
+@property (weak, nonatomic) IBOutlet UISlider *leftSlider;
+@property (weak, nonatomic) IBOutlet UISlider *bottomSlider;
+@property (weak, nonatomic) IBOutlet UIImageView *resizeImage;
+@property (weak, nonatomic) IBOutlet UISlider *rightSlider;
+@property (weak, nonatomic) IBOutlet UILabel *topLabel;
+@property (weak, nonatomic) IBOutlet UILabel *leftLabel;
+@property (weak, nonatomic) IBOutlet UILabel *bottomLabel;
+@property (weak, nonatomic) IBOutlet UILabel *rightLabel;
 @end
--- ResizeImage/ViewController.m
+++ ResizeImage/ViewController.m
@@ -18,12 +18,30 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.resizeImage.image = [UIImage imageNamed:@"message_bubble_right"];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+-(IBAction)valueChange:(id)sender {
+    
+    UISlider *slider = (UISlider*)sender;
+    UILabel *label = (UILabel*)[self.view viewWithTag:slider.tag*10];
+    label.text = [NSString stringWithFormat:@"%f", slider.value];
+    
+    self.resizeImage.image = [[UIImage imageNamed:@"message_bubble_right"] resizableImageWithCapInsets:UIEdgeInsetsMake(self.topSlider.value, self.leftSlider.value, self.bottomSlider.value, self.rightSlider.value) resizingMode:UIImageResizingModeStretch];
+}
+
+
+-(IBAction)imageHeightChange:(id)sender {
+    UISlider *slider = (UISlider*)sender;
+    CGRect imgFrame = self.resizeImage.frame;
+    self.resizeImage.frame = CGRectMake(imgFrame.origin.x, imgFrame.origin.y, imgFrame.size.width, slider.value);
 }
 
 @end
